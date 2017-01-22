596fb8ee10c076d2b83efc8ca4ec9f20e3257928
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-01-22 17:47:39 -0300

    Add raw letter mapping

--- app/serializers/rating_serializer.rb
+++ app/serializers/rating_serializer.rb
@@ -1,3 +1,13 @@
 class RatingSerializer < ActiveModel::Serializer
-  attributes :value
+  attributes :value, :letter
+
+  def letter
+    return 'F' if object.value < 0.7
+    return 'E' if object.value < 1.4
+    return 'D' if object.value < 2.1
+    return 'C' if object.value < 2.8
+    return 'B' if object.value < 3.5
+    return 'A' if object.value < 4.2
+    return 'A+' if object.value <= 5.0
+  end
 end
